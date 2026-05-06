7d581a8a47dab271552be3cded847b0e97e5dfa9
Bodix <bodix321@gmail.com>
2026-05-06 06:24:03 +0300

    Add `OnScreenSlider`

---
+++ Scripts/OnScreenSlider.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem.Layouts;
+using UnityEngine.InputSystem.OnScreen;
+using UnityEngine.UI;
+
+namespace Toolkit.InputSystem
+{
+	[AddComponentMenu("Input/On-Screen Slider")]
+	[RequireComponent(typeof(Slider))]
+	public class OnScreenSlider : OnScreenControl
+	{
+		[InputControl(layout = "Axis")]
+		[SerializeField]
+		private string _controlPath;
+
+		private Slider _slider;
+
+		protected override string controlPathInternal
+		{
+			get => _controlPath;
+			set => _controlPath = value;
+		}
+
+		private void Awake()
+		{
+			_slider = GetComponent<Slider>();
+			_slider.onValueChanged.AddListener(SendSliderValue);
+		}
+
+		private void OnDestroy()
+		{
+			if (_slider != null)
+				_slider.onValueChanged.RemoveListener(SendSliderValue);
+		}
+
+		private void SendSliderValue(float value)
+		{
+			SendValueToControl(value);
+		}
+	}
+}
