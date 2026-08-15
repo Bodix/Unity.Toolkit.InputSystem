d172d7895946a833725eee18a75206462787e486
Bodix <bodix321@gmail.com>
2026-08-15 08:00:50 +0300

    Add back navigation service. Refactoring

---
+++ Scripts/BackNavigationService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using Bodix.Evolunity.Services;
+using UnityEngine.InputSystem;
+
+namespace Toolkit.InputSystem
+{
+	public class BackNavigationService : IBackNavigationService, IDisposable
+	{
+		private readonly List<IBackHandler> _handlers = new List<IBackHandler>();
+		private readonly InputAction _backAction;
+
+		public event Action QuitRequested;
+
+		public BackNavigationService()
+		{
+			_backAction = new InputAction("Back", InputActionType.Button, "<Keyboard>/escape");
+			_backAction.AddBinding("*/{Back}");
+			_backAction.AddBinding("<Gamepad>/buttonEast");
+
+			_backAction.performed += OnBackPerformed;
+			_backAction.Enable();
+		}
+
+		public void Register(IBackHandler handler)
+		{
+			if (_handlers.Contains(handler))
+				_handlers.Remove(handler);
+
+			_handlers.Add(handler);
+		}
+
+		public void Unregister(IBackHandler handler)
+		{
+			_handlers.Remove(handler);
+		}
+
+		private void OnBackPerformed(InputAction.CallbackContext context)
+		{
+			for (int i = _handlers.Count - 1; i >= 0; i--)
+				if (_handlers[i].OnBackPressed())
+					return;
+
+			QuitRequested?.Invoke();
+		}
+
+		public void Dispose()
+		{
+			_backAction.performed -= OnBackPerformed;
+			_backAction.Disable();
+			_backAction.Dispose();
+		}
+	}
+}
